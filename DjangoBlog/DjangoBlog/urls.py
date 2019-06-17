75040087f314d6ab318681979f0a8b5d9a517693
Govind Balla <zcabgba@ucl.ac.uk>
2019-06-17 23:30:20 +0100

    styled the blog with css

--- DjangoBlog/DjangoBlog/urls.py
+++ DjangoBlog/DjangoBlog/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='blog-admin'),
     path('', include('blog.urls')),
 ]
+
+#govind00
+#TestPassword
