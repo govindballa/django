08b4e358d4692fae2e4d5cbc7becef17d070b0aa
Govind Balla <zcabgba@ucl.ac.uk>
2019-06-17 22:35:46 +0100

    created templates

--- DjangoBlog/blog/views.py
+++ DjangoBlog/blog/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Corey S',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 17 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 19 2019'
+    },
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+        'title' : 'Home Page'
+    }
+    return render(request, 'blog/home.html', context )
 
+def about(request):
+    return render(request, 'blog/about.html')
