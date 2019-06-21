afcc71a169c5f791091dbbc0eedff2036fd849cf
Govind Balla <zcabgba@ucl.ac.uk>
2019-06-21 14:25:17 +0100

    basic user login page

--- DjangoBlog/DjangoBlog/urls.py
+++ DjangoBlog/DjangoBlog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='blog-admin'),
     path('', include('blog.urls')),
+    path('register/', user_views.register, name='register'),
 ]
 
 #govind00
